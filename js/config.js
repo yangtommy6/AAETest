fe4acfc13fc9207cd54488e9cb545368f5542cd2
yangtommy6 <shiningheart0728@gmail.com>
2023-03-04 13:14:59 -0700

    nuweinf

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-west-2_qbTjlHFa4", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "79lv9a3adfbfgpfebhh06n8e8p", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-west-2_piya9jcy5", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "l8tqfj2u9vvnfce0u4f97jdpc", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-west-2", // e.g. us-east-2
   },
   api: {
