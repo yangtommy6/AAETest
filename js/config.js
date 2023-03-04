678d5a5079fb999ed08d6278f2b8f078992c0590
yangtommy6 <shiningheart0728@gmail.com>
2023-03-04 13:07:20 -0700

    ueidfnuienf

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "us-east-1", // e.g. us-east-2
+    userPoolId: "us-west-2_qbTjlHFa4", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "79lv9a3adfbfgpfebhh06n8e8p", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-west-2", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://grspkumd6j.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "https://main.d2g1x6f7l2wu7q.amplifyapp.com/", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
